5799dd0cf6d2a8b110197ead2c1196c66c2fede3
alwasega <allanwasega@gmail.com>
2019-06-30 15:30:25 +0300

    fisrt commit

---
+++ sample.py
@@ -0,0 +1,34 @@
+
+def add(x,y):
+    return x+y
+
+def subtract(a,b):
+    return a-b
+
+def multiplication(x,y,z):
+    return x*y*a
+
+def division(x,y):
+    return x/y
+
+def name(my_name):
+    if name == my_name:
+        return my_name
+    
+def absolute(x):
+    if x<0:
+        return -y
+    return x
+
+def concatenate(string_a,string_b):
+    return string_a 
+
+def number(a):
+return a
+
+
+def date (today_date):
+    
+    
+def week (todays_week):
+    return It is week 10
